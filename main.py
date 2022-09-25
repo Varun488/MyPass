4c593d17e9d1d42ec21c221730db967586868d4a
varun chourasia <varunchourasiya45@gmail.com>
2022-09-25 16:04:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,93 +1,81 @@
 from tkinter import *
 from tkinter import messagebox
 from random import choice, randint, shuffle
-import pyperclip
 import sqlite3
 from show import sh
 
-# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+def main2():
+    def generate_password():
+        letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+        numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+        symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
-#Password Generator Project
-def generate_password():
-    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+        password_letters = [choice(letters) for _ in range(randint(8, 10))]
+        password_symbols = [choice(symbols) for _ in range(randint(2, 4))]
+        password_numbers = [choice(numbers) for _ in range(randint(2, 4))]
 
-    password_letters = [choice(letters) for _ in range(randint(8, 10))]
-    password_symbols = [choice(symbols) for _ in range(randint(2, 4))]
-    password_numbers = [choice(numbers) for _ in range(randint(2, 4))]
+        password_list = password_letters + password_symbols + password_numbers
+        shuffle(password_list)
 
-    password_list = password_letters + password_symbols + password_numbers
-    shuffle(password_list)
+        password = "".join(password_list)
+        password_entry.insert(0, password)
 
-    password = "".join(password_list)
-    password_entry.insert(0, password)
-    pyperclip.copy(password)
+    db = sqlite3.connect("information.db")
+    cursor = db.cursor()
 
-#-----------------------------save in database-------------------------
-db = sqlite3.connect("information.db")
-cursor = db.cursor()
-# cursor.execute("CREATE TABLE users (websitename varchar(100) PRIMARY KEY, emailid varchar(250) NOT NULL, password varchar(250) NOT NULL UNIQUE)")
+    #cursor.execute("CREATE TABLE user(websitename varchar(100) PRIMARY KEY, emailid varchar(250) NOT NULL, password varchar(250) NOT NULL UNIQUE)")
+    def save():
+        website = website_entry.get()
+        email = email_entry.get()
+        password = password_entry.get()
 
-# ---------------------------- SAVE PASSWORD ------------------------------- #
-def save():
-
-    website = website_entry.get()
-    email = email_entry.get()
-    password = password_entry.get()
-
-    if len(website) == 0 or len(password) == 0 or len(email) == 0:
-        messagebox.showinfo(title="Oops", message="Please make sure you haven't left any fields empty.")
-    else:
-        is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: \nEmail: {email} "
+        if len(website) == 0 or len(password) == 0 or len(email) == 0:
+           messagebox.showinfo(title="Oops", message="Please make sure you haven't left any fields empty.")
+        else:
+           is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: \nEmail: {email} "
                                                       f"\nPassword: {password} \nIs it ok to save?")
         if is_ok:
            cursor.execute("""
-           INSERT INTO users(websitename,emailid,password)
+           INSERT INTO user(websitename,emailid,password)
            VALUES (?,?,?)
            """, (website,email,password))
            db.commit ()
-           db.close ()
            website_entry.delete(0, END)
            password_entry.delete(0, END)
+    
+    window = Tk()
+    window.title("Password Manager")
+    window.config(padx=50, pady=50)
 
+    canvas = Canvas(height=200, width=200)
+    logo_img = PhotoImage(file="logo.png")
+    canvas.create_image(100, 100, image=logo_img)
+    canvas.grid(row=0, column=1)
 
+    #Labels
+    website_label = Label(text="Website:")
+    website_label.grid(row=1, column=0)
+    email_label = Label(text="Email/Username:")
+    email_label.grid(row=2, column=0)
+    password_label = Label(text="Password:")
+    password_label.grid(row=3, column=0)
 
-# ---------------------------- UI SETUP ------------------------------- #
+    #Entries
+    website_entry = Entry(width=35)
+    website_entry.grid(row=1, column=1, columnspan=2)
+    website_entry.focus()
+    email_entry = Entry(width=35)
+    email_entry.grid(row=2, column=1, columnspan=2)
+    email_entry.insert(0, "varunchourasiya45@gmail.com")
+    password_entry = Entry(width=21)
+    password_entry.grid(row=3, column=1)
 
-window = Tk()
-window.title("Password Manager")
-window.config(padx=50, pady=50)
+    # Buttons
+    generate_password_button = Button(text="Generate Password", command=generate_password)
+    generate_password_button.grid(row=3, column=2)
+    add_button = Button(text="Add", width=36, command=save)
+    add_button.grid(row=4, column=1, columnspan=2)
+    check_button = Button(text="Check Saved Password", width=36, command=sh)
+    check_button.grid(row=5,column=1, columnspan=2)
 
-canvas = Canvas(height=200, width=200)
-logo_img = PhotoImage(file="logo.png")
-canvas.create_image(100, 100, image=logo_img)
-canvas.grid(row=0, column=1)
-
-#Labels
-website_label = Label(text="Website:")
-website_label.grid(row=1, column=0)
-email_label = Label(text="Email/Username:")
-email_label.grid(row=2, column=0)
-password_label = Label(text="Password:")
-password_label.grid(row=3, column=0)
-
-#Entries
-website_entry = Entry(width=35)
-website_entry.grid(row=1, column=1, columnspan=2)
-website_entry.focus()
-email_entry = Entry(width=35)
-email_entry.grid(row=2, column=1, columnspan=2)
-email_entry.insert(0, "varunchourasiya45@gmail.com")
-password_entry = Entry(width=21)
-password_entry.grid(row=3, column=1)
-
-# Buttons
-generate_password_button = Button(text="Generate Password", command=generate_password)
-generate_password_button.grid(row=3, column=2)
-add_button = Button(text="Add", width=36, command=save)
-add_button.grid(row=4, column=1, columnspan=2)
-check_button = Button(text="Check Saved Password", width=36, command=sh)
-check_button.grid(row=5,column=1, columnspan=2)
-
-window.mainloop()
+    window.mainloop()
